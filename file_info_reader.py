3c7b733107b7cc741e0b5bc52c611baa51cf07ff
jaeseoky1023 <jaeseoky1023@gmail.com>
2022-10-02 00:47:12 +0900

    inital function set

---
+++ file_info_reader.py
@@ -0,0 +1,13 @@
+import binascii
+import os, sys, datetime
+import openpyxl
+
+def get_file_info(path_dir):
+    pass
+
+if __name__ == "__main__":
+    path_dir = ""
+    
+    get_file_info(path_dir)
+    
+    
