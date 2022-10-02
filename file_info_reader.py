85511ae9998d0762d7a466f14d9391a7c9ecee1d
jaeseoky1023 <jaeseoky1023@gmail.com>
2022-10-02 23:25:46 +0900

    add function

--- file_info_reader.py
+++ file_info_reader.py
@@ -2,12 +2,21 @@ import binascii
 import os, sys, datetime
 import openpyxl
 
+file_info = []
+
 def get_file_info(path_dir):
     pass
 
+def extract_file_info():
+    pass
+
+def save_excel_file():
+    pass
 if __name__ == "__main__":
     path_dir = ""
     
     get_file_info(path_dir)
+    
+    save_excel_file
     
     
